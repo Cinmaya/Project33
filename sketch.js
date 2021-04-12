2d3e8bf4a33d5ecb731a0a4fcc1c0d7cc9983374
Cinmaya <72077672+Cinmaya@users.noreply.github.com>
2021-04-12 22:09:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,142 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Constraint = Matter.Constraint;
+const Bodies = Matter.Bodies;
+const Events = Matter.Events;
+
+var engine,world;
+var particles;
+var plinkos = [];
+var divisions=[];
+var particle;
+
+var divisionHeight=300;
+var score =0;
+var count=0;
+
+var gameState="start";
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(30)
+  text("Score : "+score,20,30);
+  text("500",15,500);
+  text("500",95,500);
+  text("500",175,500);
+  text("500",255,500);
+  text("100",335,500);
+  text("100",415,500);
+  text("100",495,500);
+  text("200",575,500);
+  text("200",655,500);
+  text("200",735,500);
+  Engine.update(engine);
+  
+  ground.display();
+
+  if(gameState=="end"){
+    textSize(100);
+    fill("red")
+    text("GAMEOVER",400,400);
+  }
+
+   for (var i = 0; i < plinkos.length; i++) {
+     plinkos[i].display();
+     
+   }
+
+   
+   if(particle!=null)
+   {
+      particle.display();
+       
+   if (particle.body.position.y>760){
+       
+    if (particle.body.position.x < 300){ 
+             
+          score=score+500;      
+          particle=null;
+                 
+       if ( count>= 5) gameState ="end";                          
+      }
+
+      else if (particle.body.position.x < 600 && particle.body.position.x > 301 ){
+      
+            score = score + 100;
+            particle=null;
+            
+            
+            if ( count>= 5) gameState ="end";
+
+    }
+    else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
+    {
+          score = score + 200;
+          particle=null;
+          
+          if ( count>= 5)  gameState ="end";
+
+    }      
+
+  }
+}
+  
+   for (var k = 0; k < divisions.length; k++)
+    {
+     divisions[k].display();
+   }
+}
+
+function mousePressed()
+{
+  if(gameState!=="end")
+  {
+    count++;
+    particle=new Particle(mouseX,10,10,10);
+  }
+}
+
+
